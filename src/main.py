80e4b4b7b446d75299f632c17f2197c15485bfe9
ashy558 <266838684+ashy558@users.noreply.github.com>
2026-06-25 14:39:49 +0100

    add return type hint to main function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+from types import NoneType
+
 from textnode import TextNode, TextType
 
 
-def main():
+def main() -> NoneType:
     print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
 
 
