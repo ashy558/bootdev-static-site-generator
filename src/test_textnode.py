3d0f0ce7e2ad60a7fbec2f4ff260c01a9ce5ee9a
ashy558 <266838684+ashy558@users.noreply.github.com>
2026-06-25 14:47:03 +0100

    add text node test

---
+++ src/test_textnode.py
@@ -0,0 +1,23 @@
+import unittest
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_text_type_ineq(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_text_content_ineq(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        node2 = TextNode("This is a different text node", TextType.PLAIN)
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
